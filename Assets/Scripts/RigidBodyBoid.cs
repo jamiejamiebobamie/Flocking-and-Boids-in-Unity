ca658b7b6bddf990be4ea27b88b87a228aba1378
jamiejamiebobamie <jmccrory@vt.edu>
2019-11-06 21:03:40 -0800

    first commit
    rigid body boids did not work

---
+++ Assets/Scripts/RigidBodyBoid.cs
@@ -0,0 +1,95 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RigidBodyBoid : MonoBehaviour
+{
+    public float minSpeed = 20.0f;
+    public float turnSpeed = 20.0f;
+    public float randomFreq = 20.0f;
+    public float randomForce = 20.0f;
+
+    //alignment variables
+    public float toOriginForce = 50.0f;
+    public float toOriginRange = 100.0f;
+    public float gravity = 2.0f;
+
+    //seperation variables
+    public float avoidanceRadius = 50.0f;
+    public float avoidanceForce = 20.0f;
+
+    //cohesion variables
+    public float followVelocity = 4.0f;
+    public float followRadius = 40.0f;
+
+    //these variables control the movement of the boid
+    private Transform origin;
+    private Vector3 velocity;
+    private Vector3 normalizedVelocity;
+    private Vector3 randomPush;
+    private Vector3 originPush;
+    private Transform[] objects;
+    private RigidBodyBoid[] otherFlocks;
+    private Transform transformComponent;
+
+    internal RigidBodyController controller;
+    private new Rigidbody rigidbody;
+
+    private void Start()
+    {
+        this.rigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (controller)
+        {
+            Vector3 relativePos = steer() * Time.deltaTime;
+            if (relativePos != Vector3.zero)
+                rigidbody.velocity = relativePos;
+
+            // enforce minimum and maximum speeds for the boids
+            float speed = rigidbody.velocity.magnitude;
+            if (speed > controller.maxVelocity)
+            {
+                rigidbody.velocity = rigidbody.velocity.normalized *
+                  controller.maxVelocity;
+            }
+            else if (speed < controller.minVelocity)
+            {
+                rigidbody.velocity = rigidbody.velocity.normalized *
+                    controller.minVelocity;
+            }
+        }
+    }
+
+    private Vector3 steer()
+    {
+        Vector3 center = controller.flockCenter -
+            transform.localPosition;  // cohesion
+        Vector3 velocity = controller.flockVelocity -
+            rigidbody.velocity;  // alignment
+        Vector3 follow = controller.target.localPosition -
+            transform.localPosition;  // follow leader
+        Vector3 separation = Vector3.zero;
+        foreach (RigidBodyBoid flock in controller.flockList)
+        {
+            if (flock != this)
+            {
+                Vector3 relativePos = transform.localPosition -
+                    flock.transform.localPosition;
+                separation += relativePos / (relativePos.sqrMagnitude);
+            }
+        }
+
+        // randomize
+        Vector3 randomize = new Vector3((Random.value * 2) - 1,
+            (Random.value * 2) - 1, (Random.value * 2) - 1);
+        randomize.Normalize();
+        return (controller.centerWeight * center +
+            controller.velocityWeight * velocity +
+            controller.separationWeight * separation +
+            controller.followWeight * follow +
+            controller.randomizeWeight * randomize);
+    }
+}
---
+++ Assets/Scripts/RigidBodyController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RigidBodyController : MonoBehaviour
+{
+    public float minVelocity = 1;  //Min Velocity
+    public float maxVelocity = 8;  //Max Flock speed
+    public int flockSize = 20;  //Number of flocks in the group
+                                //How far the boids should stick to the center (the more
+                                //weight stick closer to the center)
+
+    public float centerWeight = 1;
+    public float velocityWeight = 1;  //Alignment behavior
+                                      //How far each boid should be separated within the flock
+    public float separationWeight = 1;
+
+    //How close each boid should follow to the leader (the more
+    //weight make the closer follow)
+    public float followWeight = 1;
+
+    //Additional Random Noise
+    public float randomizeWeight = 1;
+    public RigidBodyBoid prefab;
+    public Transform target;
+
+    //Center position of the flock in the group
+    internal Vector3 flockCenter;
+    internal Vector3 flockVelocity;  //Average Velocity
+    public ArrayList flockList = new ArrayList();
+
+    void Start()
+    {
+        for (int i = 0; i < flockSize; i++)
+        {
+            RigidBodyBoid boid = Instantiate(prefab, transform.position,
+                transform.rotation) as RigidBodyBoid;
+            boid.transform.parent = transform;
+            boid.controller = this;
+            flockList.Add(boid);
+        }
+    }
+
+    void Update()
+    {
+        //Calculate the Center and Velocity of the whole flock group
+        Vector3 center = Vector3.zero;
+        Vector3 velocity = Vector3.zero;
+        foreach (RigidBodyBoid boid in flockList)
+        {
+            center += boid.transform.localPosition;
+            velocity += boid.GetComponent<Rigidbody>().velocity;
+        }
+        flockCenter = center / flockSize;
+        flockVelocity = velocity / flockSize;
+    }
+}
